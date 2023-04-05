24e44fe8de44775ba67de34d980c053bb3b32481
ogoo27 <miracleiyiagwu@gmail.com>
2023-04-05 12:56:45 +0100

    First Commit

---
+++ Client_gRPC/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7284");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "Miracle Ogoo" });
+Console.WriteLine("From server: " + reply.Message);
